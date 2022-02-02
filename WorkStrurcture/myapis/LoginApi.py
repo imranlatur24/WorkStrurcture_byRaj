789481ead52dbb84ed8cc2b9cf909527d84bc1a3
imran <imranlatur24@gmail.com>
2022-02-02 16:28:46 +0530

    login api updated with send notification by birth date

--- WorkStrurcture/myapis/LoginApi.py
+++ WorkStrurcture/myapis/LoginApi.py
@@ -12,6 +12,7 @@ import logging
 import random
 import string
 from autocorrect import Speller
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -64,36 +65,92 @@ def get_user_login():
 @cross_origin(app, support_credential=True)
 @app.route('/user_register', methods=['POST'])
 def get_user_register():
-    # try:
-    #working for android side
-    if request.method == 'POST' and 'first_name' in request.values and 'last_name' in request.values and 'empid' in request.values and 'sci_type' in request.values and 'password' in request.values and 'cpassword' in request.values:
-        first_name = request.values['first_name']
-        last_name = request.values['last_name']
-        EmpID = request.values['empid']
-        sci_type = request.values['sci_type']
-        PASSWORD = request.values['password']
-        CPASSWORD = request.values['cpassword']
-        print('first_name ', first_name)
-        print('last_name ', last_name)
-        print('EmpID ', EmpID)
-        print('sci_type ', sci_type)
-        print('PASSWORD ', PASSWORD)
-        print('CPASSWORD ', CPASSWORD)
-    conn = mysql.connect()
-    cursor = conn.cursor(pymysql.cursors.DictCursor)
-    sql = 'INSERT INTO `test`(`id`, `first_name`, `last_name`, `empid`, `sci_type`, `password`, `cpassword`) values(NULL, %s, %s,%s, %s,%s, %s)'
-    values = (first_name, last_name, EmpID, sci_type, PASSWORD, CPASSWORD)
-    cursor.execute(sql, values)
-    conn.commit()
-    print(cursor.rowcount, "record inserted.")
-    return jsonify(message="registration succesfully", false='false')
-    # except Exception as e:
-    #     print('registration api', e)
-    # finally:
-    #     cursor.close()
-    #     conn.close()
+    try:
+        #working for android side
+        if request.method == 'POST' and 'first_name' in request.values and 'last_name' in request.values and 'empid' in request.values and 'sci_type' in request.values and 'password' in request.values and 'cpassword' in request.values and 'birth_date' in request.values:
+            first_name = request.values['first_name']
+            last_name = request.values['last_name']
+            EmpID = request.values['empid']
+            sci_type = request.values['sci_type']
+            PASSWORD = request.values['password']
+            CPASSWORD = request.values['cpassword']
+            birth_date = request.values['birth_date']
+            print('first_name ', first_name)
+            print('last_name ', last_name)
+            print('EmpID ', EmpID)
+            print('sci_type ', sci_type)
+            print('PASSWORD ', PASSWORD)
+            print('CPASSWORD ', CPASSWORD)
+            print('birth_date ', birth_date)
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        sql = 'INSERT INTO `test`(`id`, `first_name`, `last_name`, `empid`, `sci_type`, `password`, `cpassword`, `birth_date`) values(NULL, %s, %s,%s, %s,%s, %s, %s)'
+        values = (first_name, last_name, EmpID, sci_type, PASSWORD, CPASSWORD,birth_date)
+        cursor.execute(sql, values)
+        conn.commit()
+        print(cursor.rowcount, "record inserted.")
+        return jsonify(message="registration succesfully", false='false')
+    except Exception as e:
+        print('registration api', e)
+    finally:
+        cursor.close()
+        conn.close()
+
+@app.route('/callChangePassword', methods=['PUT'])
+def get_cpass():
+    try:
+        if request.method == 'PUT' and 'id' in request.form and 'password' in request.form and 'cpassword' in request.form:
+            id = request.form['id']
+            PASSWORD = request.form['password']
+            cPASSWORD = request.form['cpassword']
+        # data = request.get_json()
+        # PASSWORD = data['PASSWORD']
+        # EmpID = data['EmpID']
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        sql = "UPDATE test SET password=%s and cpassword=%s WHERE id=%s"
+        values = (PASSWORD,cPASSWORD, 2)
+        cursor.execute(sql, values)
+        conn.commit()
+        return jsonify(message='password changed successfully')
+    except Exception as e:
+        print('change password api', e)
+    finally:
+        cursor.close()
+        conn.close()
 
 
+id=0
+@cross_origin(app, support_credential=True)
+@app.route('/birth_alert', methods=['GET'])
+def get_birth_alert():
+    try:
+        current_date = datetime.today().strftime('%d/%m/%Y')
+        print('current date ',current_date)
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        sql = "SELECT * FROM `test` WHERE birth_date=%s"
+        values=current_date
+        cursor.execute(sql,values)
+        result = cursor.fetchall()
+        for i in result:
+            get_date =i['birth_date']
+            id = i['id']
+            print('get date ',get_date)
+            if current_date == get_date:
+                # resp = jsonify(EmpID)
+                # resp.status_code = 200
+
+                return jsonify(message='notification sent to  ')
+            else:
+                return jsonify(message='no birthday available')
+
+            return ''
+    except Exception as e:
+        print('birth_alert api', e)
+    finally:
+        cursor.close()
+        conn.close()
 if __name__ == "__main__":
     # app.run(debug=True)
     app.run(host='192.168.0.105')
---
+++ date.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+# mydate = datetime.today().strftime('%d/%m/%Y').lstrip('0')
+# print(mydate)
+print(datetime.today().strftime('%d/%m/%Y'))
